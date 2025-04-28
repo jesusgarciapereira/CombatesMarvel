71865f75376ad682805ff7b18763f9d1c783fcc3
jgarcia <jgarcia@107-04>
2025-04-28 10:02:51 +0200

    Mejoradas las imágenes y quitados los mensajes inútiles

--- MAUI/VM/clsPuntuacionCombateVM.cs
+++ MAUI/VM/clsPuntuacionCombateVM.cs
@@ -12,7 +12,7 @@ using System.Threading.Tasks;
 
 namespace MAUI.VM
 {
-    public class clsPuntuacionCombateVM : clsCombate
+    public class clsPuntuacionCombateVM 
     {
         #region Atributos
         private List<clsLuchador> listadoLuchadores;
@@ -39,15 +39,19 @@ namespace MAUI.VM
         public clsLuchador LuchadorElegido1
         {
             get { return luchadorElegido1; }
-            set { luchadorElegido1 = value; 
-            botonGuardar.RaiseCanExecuteChanged();
+            set
+            {
+                luchadorElegido1 = value;
+                botonGuardar.RaiseCanExecuteChanged();
             }
         }
 
         public clsLuchador LuchadorElegido2
         {
             get { return luchadorElegido2; }
-            set { luchadorElegido2 = value;
+            set
+            {
+                luchadorElegido2 = value;
                 botonGuardar.RaiseCanExecuteChanged();
             }
         }
@@ -71,8 +75,8 @@ namespace MAUI.VM
         #endregion
 
         #region Constructores
-        public clsPuntuacionCombateVM() : base()
-            
+        public clsPuntuacionCombateVM()
+
         {
             puntuacionElegida1 = 1;
             puntuacionElegida2 = 1;
@@ -93,15 +97,21 @@ namespace MAUI.VM
 
         }
 
-        // creo que este constructor no hace falta
-        public clsPuntuacionCombateVM(clsCombate combate, List<clsLuchador> listadoLuchadores, int puntuacionMaxima) : base(combate.IdLuchador1, combate.IdLuchador2, combate.PuntosLuchador1, combate.PuntosLuchador2)
-        {
-            this.listadoLuchadores = listadoLuchadores;
-            this.puntuacionMaxima = puntuacionMaxima;
-        }
+        // Creo que este Constructor no hace falta
+        //public clsPuntuacionCombateVM(clsCombate combate, List<clsLuchador> listadoLuchadores, int puntuacionMaxima) : base(combate.IdLuchador1, combate.IdLuchador2, combate.PuntosLuchador1, combate.PuntosLuchador2)
+        //{
+        //    this.listadoLuchadores = listadoLuchadores;
+        //    this.puntuacionMaxima = puntuacionMaxima;
+        //}
         #endregion
 
         #region Funciones
+        /// <summary>
+        /// Muestra un mensaje emergente en la pantalla principal de la aplicación.
+        /// </summary>
+        /// <param name="titulo">Título del mensaje.</param>
+        /// <param name="cuerpo">Cuerpo del mensaje.</param>
+        /// <param name="boton">Texto del botón de cierre.</param>
         private async void muestraMensaje(string titulo, string cuerpo, string boton)
         {
             await Application.Current.MainPage.DisplayAlert(titulo, cuerpo, boton);
@@ -112,35 +122,37 @@ namespace MAUI.VM
 
         #region Comandos
         /// <summary>
-        /// Método asociado al execute del comando guardarExecute que guarda el Combate
+        /// Método asociado al execute del comando botonGuardar que guarda el Combate
         /// </summary>
         private void guardarExecute()
         {
+            // Todas estas condiciones y mensajes son innecesarios si usamos habilitarGuardar()
             bool hecho = false;
 
-            if (luchadorElegido1 == null || luchadorElegido2 == null)
+            //if (luchadorElegido1 == null || luchadorElegido2 == null)
+            //{
+            //    muestraMensaje("Error", $"Debes seleccionar a dos Luchadores", "OK");
+            //}
+            //else if (
+            //    luchadorElegido1.IdLuchador == luchadorElegido2.IdLuchador)
+            //{
+            //    muestraMensaje("Error", $"Los Luchadores seleccionados deben ser dferentes", "OK");
+            //}
+            //else
+            //{
+            try
             {
-                muestraMensaje("Error", $"Debes seleccionar a dos Luchadores", "OK");
-            }
-            else if (
-                luchadorElegido1.IdLuchador == luchadorElegido2.IdLuchador)
-            {
-                muestraMensaje("Error", $"Los Luchadores seleccionados deben ser dferentes", "OK");
-            }
-            else
-            {
-                try
-                {
-                    // No sé si es así
-                    combate = new clsCombate(luchadorElegido1.IdLuchador, luchadorElegido2.IdLuchador, puntuacionElegida1, puntuacionElegida2);
+                combate = new clsCombate(luchadorElegido1.IdLuchador, luchadorElegido2.IdLuchador, puntuacionElegida1, puntuacionElegida2);
 
-                    hecho = clsManejadoraCombatesBL.GuardarCombateBL(combate);
-                }
-                catch (SqlException e)
-                {
-                    muestraMensaje("Error", "Ha habido un problema en la Base de Datos, vuelva a intentarlo más tarde", "OK");
-                }
+                hecho = clsManejadoraCombatesBL.GuardarCombateBL(combate);
+
+                //Creo que aquí deberíamos actualizar la clasificación
             }
+            catch (SqlException e)
+            {
+                muestraMensaje("Error", "Ha habido un problema en la Base de Datos, vuelva a intentarlo más tarde", "OK");
+            }
+            //}
 
             if (hecho)
             {
@@ -148,6 +160,10 @@ namespace MAUI.VM
             }
         }
 
+        /// <summary>
+        /// Método asociado al canExecute del comando botonGuardar que habilita o deshabilita el botón de guardar.
+        /// </summary>
+        /// <returns>True si se han seleccionado dos luchadores diferentes, false en caso contrario.</returns>
         private bool habilitarGuardar()
         {
             bool habilitado = false;
