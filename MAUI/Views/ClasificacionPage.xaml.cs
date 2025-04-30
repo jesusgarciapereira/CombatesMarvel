cfd1d72953725787aad713842644f450c1e4ff51
jgarcia <jgarcia@107-04>
2025-04-30 12:23:43 +0200

    A la espera de Fernando

--- MAUI/Views/ClasificacionPage.xaml.cs
+++ MAUI/Views/ClasificacionPage.xaml.cs
@@ -5,9 +5,11 @@ namespace MAUI.Views;
 
 public partial class ClasificacionPage : ContentPage
 {
+    #region Atributos
     clsClasificacionVM miVM;
+    #endregion
 
-
+    #region Constructores
     public ClasificacionPage()
 	{
 		InitializeComponent();
@@ -15,11 +17,18 @@ public partial class ClasificacionPage : ContentPage
         miVM = (clsClasificacionVM)this.BindingContext;
 
     }
+    #endregion
 
+    #region Métodos
+    /// <summary>
+    /// Método que se ejecuta cuando la página aparece en pantalla.
+    /// Sobreescritura: Llama al método para actualizar la clasificación de luchadores.
+    /// </summary>
     protected override void OnAppearing()
     {
         base.OnAppearing();
 
         miVM.actualizarClasificacion();
     }
+    #endregion
 }
